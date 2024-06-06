5e4fd3b5b8a1bbefe76a3b10ff74e1f61856a63e
BrunoMalfi <malfi.bruno@gmail.com>
2024-06-06 16:29:32 +0200

    added connection to Mongo/atlas

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const app = express();
-
+const { dbConnection } = require('./config/config.js');
+const cors = require('cors');
+app.use(express.json());
+app.use(cors());
+dbConnection();
 
 
 // app.get('/api/news/', async (req, res) => {
